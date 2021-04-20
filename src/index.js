8b395bc585430fa4d8fee69e16495d2fb062f281
eax <goo.eax@gmail.com>
2021-04-20 16:02:11 +0300

    add no-relative-parent-imports rule

---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'no-relative-parent-imports': require('./rules/no-relative-parent-imports'),
+  },
+};
---
+++ src/rules/no-relative-parent-imports.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const moduleVisitor = require('eslint-module-utils/moduleVisitor').default;
+const resolve = require('eslint-module-utils/resolve').default;
+
+
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: '',
+      category: '',
+      recommended: true,
+      url: '',
+    },
+    fixable: 'code',
+    schema: [{
+      'type': 'object',
+      'properties': {
+        'baseUrl': {
+          'type': 'string',
+        },
+      },
+      'additionalProperties': false,
+    }],
+  },
+
+  create: function noRelativePackages(context) {
+    const filePath = context.getFilename();
+
+    const rootDir = context.getCwd();
+    const baseUrlConfig = context.options[0].baseUrl;
+    const baseUrl = baseUrlConfig && path.join(rootDir, baseUrlConfig);
+
+    // can't check a non-file
+    if (filePath === '<text>') {
+      return {};
+    }
+
+    function checkSourceValue(sourceNode) {
+      const depPath = sourceNode.value;
+
+      if (!depPath || !depPath.startsWith('../')) {
+        return;
+      }
+
+      const absDepPath = resolve(depPath, context);
+
+      // unable to resolve path
+      if (!absDepPath) {
+        return;
+      }
+
+      if (baseUrl) {
+        const pathRelativeToBaseUrl = path.join(path.dirname(filePath), depPath).replace(baseUrl + '/', '');
+
+        context.report({
+          node: sourceNode,
+          message: 'no relative parent imports',
+          fix: function (fixer) {
+            return fixer.replaceText(sourceNode, JSON.stringify(pathRelativeToBaseUrl));
+          },
+        });
+      }
+    }
+
+    return moduleVisitor(checkSourceValue, context.options[0]);
+  },
+};
